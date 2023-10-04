2be85b6c0996ae2d58440e889d74084eb78d782e
luke-teo <yipengteo.luke@gmail.com>
2023-10-04 21:05:31 +0900

    feat: added file browser plugin for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,13 +1,70 @@
 require('telescope').setup({
+	pickers = {
+		find_files = {
+			find_command = {
+				'fd',
+				'--type',
+				'f',
+				'--color=never',
+				'--hidden',
+				'--follow',
+				'-E',
+				'.git/*',
+			},
+		},
+		git_files = {
+			find_command = {
+				'fd',
+				'--type',
+				'f',
+				'--color=never',
+				'--hidden',
+				'--follow',
+				'-E',
+				'.git/*',
+			},
+		},
+		grep_string = {
+			find_command = {
+				'fd',
+				'--type',
+				'f',
+				'--color=never',
+				'--hidden',
+				'--follow',
+				'-E',
+				'.git/*',
+			},
+		},
+		live_grep = {
+			find_command = {
+				'fd',
+				'--type',
+				'f',
+				'--color=never',
+				'--hidden',
+				'--follow',
+				'-E',
+				'.git/*',
+			},
+		},
+	},
 	extensions = {
     	fzf = {
-      	fuzzy = true,                    -- false will only do exact matching
-      	override_generic_sorter = true,  -- override the generic sorter
-      	override_file_sorter = true,     -- override the file sorter
-      	case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    	}
+			fuzzy = true,                    -- false will only do exact matching
+			override_generic_sorter = true,  -- override the generic sorter
+			override_file_sorter = true,     -- override the file sorter
+			case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+										   -- the default case_mode is "smart_case"
+    	},
+		file_browser = {
+			theme = "dropdown",
+			hijack_netrw = true,
+			hidden = true,
+			previewer = false,
+		},
   	}
 })
 
 require('telescope').load_extension('fzf')
+require('telescope').load_extension('file_browser')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,86 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("pluginlist")
+return {
+	-- UI 
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme kanagawa")
+		end
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons"
+		},
+		config = function()
+			require("lualine").setup({
+				icons_enabled = true,
+				theme = "onedark",
+			})
+		end,
+	},
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			vim.notify = require("notify")
+		end
+	},
+	-- LSP/CMP
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"folke/neodev.nvim",
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
+			'rafamadriz/friendly-snippets',
+			'hrsh7th/cmp-nvim-lsp',
+		},
+	},
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+	},
+	-- File
+	{
+		'nvim-telescope/telescope.nvim',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		'nvim-telescope/telescope-fzf-native.nvim',
+		build = 'make',
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+	},
+	-- Git
+	"lewis6991/gitsigns.nvim",
+	{
+		'kdheepak/lazygit.nvim',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+		},
+		config = function()
+			require("telescope").load_extension("lazygit")
+		end,
+	},
+	-- Editting Supports
+	{
+		'numToStr/Comment.nvim',
+		opts = {
+			toggler = {
+				line = '<leader>/',
+			},
+		},
+		lazy = false,
+	},
+	{
+		'Pocco81/auto-save.nvim',
+		config = function()
+			require("auto-save").setup{}
+		end,
+	},
+}
